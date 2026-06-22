4fb17241be60d79d4b6bc4e5898cc9a2f161b744
arieltizimedina123-wq <arieltizimedina123@gmail.com>
2026-06-21 21:59:04 -0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "TU_TOKEN"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot online 🤖")
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+print("Bot listo...")
+app.run_polling()
